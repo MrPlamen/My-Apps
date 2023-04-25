3668f8ede79de924fdafc0a79e453b19879bfc3e
Plamen Petrov <99687005+MrPlamen@users.noreply.github.com>
2023-04-25 14:20:45 +0300

    Add files via upload

---
+++ DiffCheckerSite/checker.js
@@ -0,0 +1,43 @@
+function diffChecker() {
+    const originalText = document.getElementById('original-text');
+    const comparedText = document.getElementById('compared-text');
+    const submitBtn = document.getElementById('submit-btn');
+
+    submitBtn.addEventListener("click", compareTexts);
+
+    function compareTexts(event) {
+        let textOne = originalText.textContent;
+        let textTwo = comparedText.textContent;
+        let loopLength = textTwo.length;
+        let textMemory ='';
+
+        if (textOne.length > textTwo.length) {
+            loopLength = textOne.length;
+        }
+
+        for (let i = 0; i < loopLength; i++) {
+            let charOne = textOne[i];
+            let charTwo = textTwo[i];
+            let charOneNext = textOne[i+1];
+            let charTwoNext = textTwo[i+1];
+            let charOnePrev = textOne[i-1];
+            let charTwoPrev = textTwo[i-1];
+           
+            if (charOne === charTwo) {
+                textMemory += charTwo;
+            } else {
+                if ((charOnePrev && charTwoPrev) && (charOnePrev === charTwoPrev)) {
+                    textMemory += '<span class="diff">';                     
+                }
+                textMemory += charTwo;              
+                if ((charOneNext && charTwoNext) && (charOneNext === charTwoNext)) {
+                    textMemory += '</span>'
+                }
+            }
+        }
+        comparedText.value = textMemory;
+        comparedText.innerHTML = textMemory;
+    }
+}
+
+diffChecker();
