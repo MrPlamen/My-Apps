acf15e92557127a660afee1c90bb0e148f93f733
Plamen Petrov <99687005+MrPlamen@users.noreply.github.com>
2023-05-13 23:56:38 +0300

    Update app.js

--- Calculator/app.js
+++ Calculator/app.js
@@ -12,7 +12,7 @@ function Calculator() {
         btn.addEventListener("click", function () {
             if (trigger) {
                 screen.textContent = '';
-                trigger = true;
+                trigger = false;
             }
 
             if (btn.textContent === 'C') {
